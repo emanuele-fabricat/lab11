ab4879a71ba554688b32ddc441ddeba6068ef177
Emanuele Fabricat - emanuele.fabricat@studio.unibo.it <emanuele.fabricat@studio.unibo.it>
2024-12-02 11:10:46 +0100

    method orderedSongNames done

--- 111-stream-utilities-for-music-model/src/main/java/it/unibo/oop/lab/streams/MusicGroupImpl.java
+++ 111-stream-utilities-for-music-model/src/main/java/it/unibo/oop/lab/streams/MusicGroupImpl.java
@@ -31,11 +31,12 @@ public final class MusicGroupImpl implements MusicGroup {
 
     @Override
     public Stream<String> orderedSongNames() {
-        return null;
+        return this.songs.stream().map(Song :: getSongName).sorted();
     }
 
     @Override
     public Stream<String> albumNames() {
+        
         return null;
     }
 
