9e2fbc9f280dfaf5cf457d9b6ec7bc973e0a87da
Emanuele Fabricat - emanuele.fabricat@studio.unibo.it <emanuele.fabricat@studio.unibo.it>
2024-12-02 11:48:15 +0100

    method countSongsInNoAlbum done

--- 111-stream-utilities-for-music-model/src/main/java/it/unibo/oop/lab/streams/MusicGroupImpl.java
+++ 111-stream-utilities-for-music-model/src/main/java/it/unibo/oop/lab/streams/MusicGroupImpl.java
@@ -52,7 +52,7 @@ public final class MusicGroupImpl implements MusicGroup {
 
     @Override
     public int countSongsInNoAlbum() {
-        return -1;
+        return (int) this.songs.stream().filter(s -> s.getAlbumName().isEmpty()).count();
     }
 
     @Override
